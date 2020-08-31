68737428664dceacb462da2a12a2b4436f327001
Antony <antony.johnpeter@pearson.com>
2020-08-31 20:58:40 +0530

    updated

--- Assignments/JDBC/employee/EmployeeDB.java
+++ Assignments/JDBC/employee/EmployeeDB.java
@@ -1,13 +1,16 @@
 package employee;
 
+import java.awt.List;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.ListIterator;
 import java.util.Map;
 import java.util.Map.Entry;
 
@@ -96,7 +99,7 @@ public class EmployeeDB {
 	            		+ "Date,"
 	            		+ "EmployeeID, "
 	            		+ "SalaryAmount) values ( "
-	            		+ "getdate(),?, ? ");
+	            		+ "getdate(),?, ? )");
 	            
 	            Iterator<Entry<Integer, Double>> it = myMap.entrySet().iterator();
 	            
@@ -107,9 +110,7 @@ public class EmployeeDB {
 	                stmt.addBatch(); // this will just collect the data values
 	                it.remove();
 	            }	            
-	            System.out.println(stmt);
-
-	            stmt.executeUpdate();
+	            stmt.executeBatch();
            	System.out.println("Action is successful!");
 	        }
 	        // Handle any errors that may have occurred.
@@ -118,4 +119,171 @@ public class EmployeeDB {
 	            
 	        }
  }
+	
+	public static void viewMonthlySalary() {
+		
+		 //public static void main(String[] args) {
+		boolean bFound = false;
+		double amount = 0;
+	    HashMap<Integer, Double> myMap = new HashMap<Integer, Double>();
+		
+		 int customerID = 0;
+		   String connectionUrl =
+		                "jdbc:sqlserver://CHE-2V4WMV2\\SQLEXPRESS:50232"
+		                        + ";database=Employee"
+		                        + ";integratedSecurity=true";		        
+		   ResultSet resultSet = null;
+
+	        try (Connection connection = DriverManager.getConnection(connectionUrl);        		
+	        		Statement statement = connection.createStatement();) {
+	        	// Create and execute a SELECT SQL statement.
+	        	String selectSql = "select m.ID,e.EmployeeUID,e.Name,t.Type,m.SalaryAmount,m.BonusAmount,CONVERT(varchar,m.date,100) as date from Employee e" + 
+	        			" Inner join MonthlySalary m " + 
+	        			" on e.ID = m.EmployeeID"+
+	        			" Inner Join EmployeeType t" +
+	        			" on" +
+	        			" t.ID = e.EmployeeTypeID" +
+	        			" where m.date in (select MAX(Date) as myDate from MonthlySalary GROUP BY EmployeeID )";
+	            resultSet = statement.executeQuery(selectSql);	
+	            while (resultSet.next()) {            	
+	            	bFound = true;     
+		            System.out.println("----------------------------------");
+	            	System.out.println("Employee UID --> " + resultSet.getString("EmployeeUID"));
+	            	System.out.println("Employee Name --> " + resultSet.getString("Name"));
+	            	System.out.println("Employee Type --> " + resultSet.getString("Type"));
+	            	System.out.println("Salary Amount --> " + resultSet.getString("SalaryAmount"));
+	            	System.out.println("Bonus Amount --> " + resultSet.getString("BonusAmount"));
+	            	System.out.println("Date --> " + resultSet.getString("date"));	            		            	 
+	            }	            
+	            System.out.println("----------------------------------");            
+	            if (!(bFound)) {
+	            	throw new SQLException ("No Salary information found ! ");
+	            }
+          	System.out.println("Action is successful!");
+	        }
+	        // Handle any errors that may have occurred.
+	        catch (SQLException e) {	        	
+	            e.printStackTrace();
+	            
+	        }
+}
+	
+	public static void processDOBBonus() {
+		
+		 //public static void main(String[] args) {
+		boolean bFound = false;
+		double amount = 0;
+		double bonus = 1000;
+		ArrayList<Integer> myList = new ArrayList<Integer>();
+		
+		 int customerID = 0;
+		   String connectionUrl =
+		                "jdbc:sqlserver://CHE-2V4WMV2\\SQLEXPRESS:50232"
+		                        + ";database=Employee"
+		                        + ";integratedSecurity=true";		        
+		   ResultSet resultSet = null;
+
+	        try (Connection connection = DriverManager.getConnection(connectionUrl);        		
+	        		Statement statement = connection.createStatement();) {
+	        	// Create and execute a SELECT SQL statement.
+	        	String selectSql = "select m.ID,e.EmployeeUID,e.Name,m.SalaryAmount from Employee e"
+	        			+" Inner join MonthlySalary m"
+	        			+" on e.ID = m.EmployeeID"
+	        			+" where m.date in (select MAX(Date) as myDate from MonthlySalary"
+	        			+" GROUP BY EmployeeID ) and  MONTH(e.DOB) = Month(getdate())";	        	
+	            resultSet = statement.executeQuery(selectSql);
+	            while (resultSet.next()) {            	
+	            	bFound = true;	            	       	
+	            	myList.add(Integer.parseInt(resultSet.getString("ID")));	            	
+	            }
+	            
+	            if (!(bFound)) {
+	            	throw new SQLException ("No Employees have their DOB this month!");
+	            }
+	            
+	            PreparedStatement stmt = connection.prepareStatement("Update MonthlySalary set BonusAmount = "+bonus+" where ID =  ?");
+	            		
+	            
+	            ListIterator <Integer> it = myList.listIterator();
+	            
+	            while (it.hasNext()) {	                
+	                stmt.setInt(1, it.next());               
+	                stmt.addBatch(); // this will just collect the data values
+	                it.remove();
+	            }	            
+
+	            stmt.executeBatch();
+          	System.out.println("Action is successful!");
+	        }
+	        // Handle any errors that may have occurred.
+	        catch (SQLException e) {	        	
+	            e.printStackTrace();
+	            
+	        }
+}
+	public static void processTypeAndSalesBonus(String employeeType, double salesThrehold) {
+		
+		 //public static void main(String[] args) {
+		boolean bFound = false;
+		double amount = 0;
+		double bonus = 1000;
+		ArrayList<Integer> myList = new ArrayList<Integer>();
+		
+		 int customerID = 0;
+		   String connectionUrl =
+		                "jdbc:sqlserver://CHE-2V4WMV2\\SQLEXPRESS:50232"
+		                        + ";database=Employee"
+		                        + ";integratedSecurity=true";		        
+		   ResultSet resultSet = null;
+
+	        try (Connection connection = DriverManager.getConnection(connectionUrl);        		
+	        		Statement statement = connection.createStatement();) {
+	        	// Create and execute a SELECT SQL statement.
+	        	String selectSql = "select m.ID,e.EmployeeUID,e.Name,t.Type,m.SalaryAmount,m.BonusAmount,CONVERT(varchar,m.date,100) as date from Employee e"
+	        			+ " Inner join MonthlySalary m"
+	        			+ " on e.ID = m.EmployeeID"
+	        			+ " Inner Join EmployeeType t"
+	        			+ " on"
+	        			+ " t.ID = e.EmployeeTypeID"
+	        			+ " where m.date in (select MAX(Date) as myDate from MonthlySalary"
+	        			+ " GROUP BY EmployeeID) and t.type = '"+employeeType+"' and m.EmployeeID in ("
+	        			+ " select s.EmployeeID  from sales s"
+	        			+ " inner join employee e"
+	        			+ " on" 
+	        			+ " e.id = s.EmployeeID"
+	        			+ " where Month(s.date) = Month(getdate())" 
+	        			+ " Group by s.EmployeeID"
+	        			+ " Having SUM(s.Amount)>"+salesThrehold+")"
+;
+	        	System.out.println(selectSql);
+	            resultSet = statement.executeQuery(selectSql);
+	            while (resultSet.next()) {            	
+	            	bFound = true;	            	       	
+	            	myList.add(Integer.parseInt(resultSet.getString("ID")));           	 
+	            }
+	            
+	            if (!(bFound)) {
+	            	throw new SQLException ("No Employees found for processing bonus");
+	            }
+	            
+	            PreparedStatement stmt = connection.prepareStatement("Update MonthlySalary set BonusAmount = "+bonus+" where ID =  ?");
+	            		
+	            
+	            ListIterator <Integer> it = myList.listIterator();
+	            
+	            while (it.hasNext()) {	                
+	                stmt.setInt(1, it.next());               
+	                stmt.addBatch(); // this will just collect the data values
+	                it.remove();
+	            }	            
+
+	            stmt.executeBatch();
+         	System.out.println("Action is successful!");
+	        }
+	        // Handle any errors that may have occurred.
+	        catch (SQLException e) {	        	
+	            e.printStackTrace();
+	            
+	        }
+}
 }
--- Assignments/JDBC/employee/TestEmployeeData.java
+++ Assignments/JDBC/employee/TestEmployeeData.java
@@ -9,15 +9,18 @@ public class TestEmployeeData {
 		boolean bFull=true;
 		String option="";
 		double percentage=0;
-		int employeeTypeID = 0;
+		String employeeType = "";
 		Scanner s = new Scanner(System.in);
 		EmployeeInfo employeeInfo = new EmployeeInfo();
 		EmployeeDB employeeDB = new EmployeeDB();
+		double salesThrehold = 0;
 		while (bFull) {
 			System.out.println("--Employee PayRoll Management--");
-			System.out.println("Enter 1 to Add Increment based on EmployeeType");
-			System.out.println("Enter 2 to Add Bonus based on EmployeeType and Sales");
-			System.out.println("Enter 3 to Process Salary");			
+			System.out.println("Enter 1 to Add Increment to Salary based on EmployeeType");
+			System.out.println("Enter 2 to Process Salary");
+			System.out.println("Enter 3 to Process Bonus based on EmployeeType and Sales");
+			System.out.println("Enter 4 to Process Bonus based on Employee DOB");	
+			System.out.println("Enter 5 to View Salary");	
 			option = s.next();
 			switch(option) {
 			case "1" :
@@ -27,22 +30,34 @@ public class TestEmployeeData {
 				System.out.println("Enter 3 to Choose TieredCommision");
 				System.out.println("Enter 4 to Choose RevenueCommision");
 				System.out.println("Enter 5 to Choose GrossMarginCommission");
-				employeeTypeID = s.nextInt();				
+				employeeType =getEmployeeType(s.nextInt()) ;				
 				System.out.println("Enter increment percentage");
 				percentage = s.nextDouble();
-				EmployeeDB.addSalaryRaise(getEmployeeType(employeeTypeID), percentage);				
+				EmployeeDB.addSalaryRaise(employeeType, percentage);				
 				break;
-			case "2" :
+			case "2" :				
+				EmployeeDB.processSalary();
+				break;
+			case "3":
 				System.out.println("Choose an EmployeeType from below");
 				System.out.println("Enter 1 to Choose FixedCommission");
 				System.out.println("Enter 2 to Choose BasePlusCommision");
 				System.out.println("Enter 3 to Choose TieredCommision");
 				System.out.println("Enter 4 to Choose RevenueCommision");
 				System.out.println("Enter 5 to Choose GrossMarginCommission");
-				employeeTypeID = s.nextInt();	
-				break;
-			case "3":
+				employeeType = getEmployeeType(s.nextInt());
+				System.out.println("Enter Sales Threhold : ");
+				salesThrehold = s.nextDouble();		
 				EmployeeDB.processSalary();
+				EmployeeDB.processTypeAndSalesBonus(employeeType, salesThrehold);
+				break;
+			case "4":
+				EmployeeDB.processSalary();
+				EmployeeDB.processDOBBonus();
+				
+				break;
+			case "5":
+				EmployeeDB.viewMonthlySalary();
 				break;
 			}
 				
